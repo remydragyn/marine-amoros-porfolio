36ca0f8c0d73bc6d8adeac9c328c59610a4173a4
Remy Dragyn <remy.dragyn@gmail.com>
2017-10-30 18:44:36 +0100

    Manu Js started

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,17 @@
-import TweenMax from 'gsap'
+// import TweenMax from 'gsap'
 import $ from 'zepto-webpack'
 
-console.log($)
+var $subPartName = $('.sub-part-name-js')
+var $subNavItem = $('.sub-nav-item-js')
 
-TweenMax.to('.name.title', 1, {x: 60, delay: 3})
+function onMouseEnterSubPartName () {
+  var $this = $(this)
+
+  console.log(this)
+  $subNavItem.removeClass('hover')
+  $this.parents('.sub-nav-item-js').addClass('hover')
+}
+
+$(window).on('load', function () {
+  $subPartName.on('mouseenter', onMouseEnterSubPartName)
+})
